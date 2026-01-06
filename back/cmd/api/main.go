2239f5514d45e94d935941521756ea03e8860dd0
Martial <floquet.martial@gmail.com>
2026-01-06 13:56:02 +0100

    fix(main.go): add minimal server

--- back/cmd/api/main.go
+++ back/cmd/api/main.go
@@ -1,1 +1,15 @@
 package main
+
+import(
+	"fmt"
+	"net/http"
+)
+
+func main(){
+http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, Go!")
+})
+
+http.ListenAndServe(":3000", nil)
+
+}
